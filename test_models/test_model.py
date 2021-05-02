197073f0d1a1a41943407fa5588b9f909df3dd00
Jacob Almrot <jacob.almrot@gmail.com>
2021-05-02 12:55:37 +0200

    Update

--- test_models/test_model.py
+++ test_models/test_model.py
@@ -12,6 +12,7 @@ import models.models.pred_logistic_regression as pred_logistic_regression
 import models.models.pred_KNN as pred_KNN
 
 # Fixa mina modeller med "pipe" och korrekt skalning https://scikit-learn.org/stable/modules/preprocessing.html
-# Bra post, https://stackoverflow.com/questions/38077190/how-to-increase-the-model-accuracy-of-logistic-regression-in-scikit-python
+# Bra post, https://stackoverflow.com/questions/38077190/how-to-increase-the-model-accuracy-of-logistic-regression-in-scikit-python 
+# Tror jag har missat att skala om datan för slutgiltiga prediction
 f = open
 

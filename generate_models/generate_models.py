15ad094c85b3ca7add9383c5b93efd7c365eecaf
Jacob <jacal517@student.liu.se>
2021-05-23 19:16:57 +0200

    Created models for SVC

--- generate_models/generate_models.py
+++ generate_models/generate_models.py
@@ -2,24 +2,12 @@
 import argh
 import json
 from tqdm import tqdm
-
-from handler import api
+import os
 
 import models.model_SVC as model_SVC 
 from models.eval_model import print_eval
 from models.eval_model import unit_bet
-
-api = api("https://statsapi.web.nhl.com/api/v1/", True, True)
-
-def get_num_shots(gamePk, player_id):
-    response_json = api.send_request("game/"+str(gamePk)+"/boxscore")
-    num_shots = None
-    if "ID" + str(player_id) in response_json["teams"]["home"]["players"]:
-        num_shots = response_json["teams"]["home"]["players"]["ID" + str(player_id)]["stats"]["skaterStats"]["shots"]
-
-    if "ID" + str(player_id) in response_json["teams"]["away"]["players"]:
-        num_shots = response_json["teams"]["away"]["players"]["ID" + str(player_id)]["stats"]["skaterStats"]["shots"]
-    return num_shots
+from joblib import dump, load
 
 
 def split_average_odds(average_odds):
@@ -34,44 +22,60 @@ def split_average_odds(average_odds):
 def generate_models(allModels=False, SVC=False):
     f = open("./data/input_tmp.txt")
     files = f.readlines()
+    f.close()
     #Read bet info
     f = open('./data/player_ids.json',)
     player_ids = json.load(f)
+    f.close()
     f = open('./data/all_bets.json',)
     all_bets = json.load(f)
+    f.close()
+    f = open('./generated_models/model_index.json')
+    res = json.load(f)
+    f.close()
     # Variables
-    model_edge = 0.3
-    
-    res = {}
+    model_edge = 0.3    
+
     for tmp in tqdm(files):
         file, player_id, earliest_gamePk_index, over_unders, average_odds = tmp.split(" ")
         average_odds = split_average_odds(average_odds)
+        res[file] = {}
+        res[file]["average_odds"] = average_odds
+        res[file]["player_id"] = player_id
+        player_model_path = "./generated_models/" + all_bets[player_id]["player_name"].lower().replace(" ", "_") +"/"
+
+        if not os.path.exists(player_model_path):
+            os.makedirs(player_model_path)
+
         for over_under in over_unders.split(","):
             over_under = over_under.replace("\n", "")
+
             if(allModels):
                 SVC = True
             
             if(SVC):
-                res_under = model_SVC.generate_model(file, "shots_this_game_U"+str(over_under), int(earliest_gamePk_index), average_odds[over_under + "_under"], model_edge)
-                res_over = model_SVC.generate_model(file, "shots_this_game_O"+str(over_under), int(earliest_gamePk_index), average_odds[over_under + "_over"], model_edge)
+                model_name = "SVC"
+                model_under = model_SVC.generate_model(file, "shots_this_game_U"+str(over_under), int(earliest_gamePk_index), average_odds[over_under + "_under"], model_edge)
+                model_over = model_SVC.generate_model(file, "shots_this_game_O"+str(over_under), int(earliest_gamePk_index), average_odds[over_under + "_over"], model_edge)
+                if model_name not in res[file]:
+                    res[file][model_name] = {}
+                
+                model_under_filepath = player_model_path + model_name + "_under_" + str(over_under) + ".joblib" 
+                model_over_filepath = player_model_path + model_name + "_over_" + str(over_under) + ".joblib"
 
-                if((average_odds[over_under + "_under"] - (1/float(res_under["precision accuracy"]))) > model_edge):
-                    if file not in res:
-                        res[file] = {}
-                        res[file]["player_id"] = player_id
-                        res[file]["preds"] = {}
-                    res[file]["preds"][str(over_under)+"_under"] = res_under
+                res[file][model_name]["shots_this_game_U"+str(over_under)] = model_under_filepath
+                res[file][model_name]["shots_this_game_O"+str(over_under)] = model_over_filepath
 
-                if((average_odds[over_under + "_over"] - (1/float(res_over["precision accuracy"]))) > model_edge):
-                    if file not in res:
-                        res[file] = {}
-                        res[file]["player_id"] = player_id
-                        res[file]["preds"] = {}
-                    res[file]["preds"][str(over_under)+"_over"] = res_over
+                dump(model_under, model_under_filepath)
+                dump(model_over, model_over_filepath)
+
             #break
         #break
-    #print_eval(res)
-    print("Result from unit bet: {}".format(unit_bet(2, res, all_bets)))
+    # Save a json object with information about paths for models and the precision of each model.
+    json_object = json.dumps(res, indent = 4)
+    with open("./generated_models/model_index.json", "w") as outfile:
+        outfile.write(json_object)
+
 
 if __name__ == "__main__":
     parser=argh.ArghParser()
--- testing/test.py
+++ testing/test.py
@@ -1,42 +1,17 @@
-from sklearn.manifold import TSNE
-from keras.datasets import mnist
-from sklearn.datasets import load_iris
-from numpy import reshape
-import seaborn as sns
-import pandas as pd
-
-iris = load_iris()
-x = iris.data
-y = iris.target
-
-tsne = TSNE(n_components=2, verbose=1, random_state=123)
-z = tsne.fit_transform(x)
-df = pd.DataFrame()
-df["y"] = y
-df["comp-1"] = z[:,0]
-df["comp-2"] = z[:,1]
-
-sns.scatterplot(x="comp-1", y="comp-2", hue=df.y.tolist(),
-                palette=sns.color_palette("hls", 3),
-                data=df).set(title="Iris data T-SNE projection")
-"""
-(x_train, y_train), (_ , _) = mnist.load_data()
-x_train = x_train[:3000]
-y_train = y_train[:3000]
-print(x_train.shape) 
- 
-x_mnist = reshape(x_train, [x_train.shape[0], x_train.shape[1]*x_train.shape[2]])
-print(x_mnist.shape)
-
-tsne = TSNE(n_components=2, verbose=1, random_state=123)
-z = tsne.fit_transform(x_mnist)
- 
-df = pd.DataFrame()
-df["y"] = y_train
-df["comp-1"] = z[:,0]
-df["comp-2"] = z[:,1]
-
-sns.scatterplot(x="comp-1", y="comp-2", hue=df.y.tolist(),
-                palette=sns.color_palette("hls", 10),
-                data=df).set(title="MNIST data T-SNE projection")
-"""                
+from pandas import read_csv
+from sklearn.feature_selection import RFE
+from sklearn.linear_model import LogisticRegression
+# load data
+url = "https://raw.githubusercontent.com/jbrownlee/Datasets/master/pima-indians-diabetes.csv"
+names = ['preg', 'plas', 'pres', 'skin', 'test', 'mass', 'pedi', 'age', 'class']
+dataframe = read_csv(url, names=names)
+array = dataframe.values
+X = array[:,0:8]
+Y = array[:,8]
+# feature extraction
+model = LogisticRegression(solver='lbfgs')
+rfe = RFE(model, 3)
+fit = rfe.fit(X, Y)
+print("Num Features: %d" % fit.n_features_)
+print("Selected Features: %s" % fit.support_)
+print("Feature Ranking: %s" % fit.ranking_)
